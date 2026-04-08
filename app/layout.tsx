6e051c2f343f53239a37880fa67410bc1200772e
Kris <kris@ironcladbimsolutions.com>
2026-04-07 23:00:59 -0400

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-W47LZQ4MHY);
+            gtag('config', 'G-W47LZQ4MHY');
           `}
         </Script>
 
